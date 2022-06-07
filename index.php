3e030933345c7d1047954da06c3bb9cfbb42567d
Frédéric NOËL <contact@fredericnoel.com>
2022-06-07 12:28:12 +0200

    Calcul longueur et largeur d'un écran 16/9

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 
-function afficherMessage() {
-    echo "<p>Je suis un message</p>";
+function afficherMessage($prenom) {
+    $resultat = "<p>Bonjour " . $prenom . "</p>";
+    return $resultat;
 }
 
-afficherMessage();
-afficherMessage();
-afficherMessage();
+function calculerSurface(int|float $longueur, int|float $largeur) : float
+{
+    return abs($longueur * $largeur);
+}
+
+function calculerLongueurLargeur(float $diagonale) : string
+{
+    $diagonale = 2.54 * abs($diagonale);
+    $longueur = round($diagonale / sqrt(1 + (9/16) ** 2 ), 2);
+    $largeur = round((9 * $longueur) / 16, 2);
+    $longueur = str_replace('.', ',', $longueur);
+    $largeur = str_replace('.', ',', $largeur);
+    $resultat = "Longueur : $longueur cm, largeur : $largeur cm";
+    return $resultat;
+}
+
+echo calculerLongueurLargeur(50);
